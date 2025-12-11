b867b6bfadeb1910f2130b9265abcfd67125ecb3
cristian <cristiancocabjn@gmail.com>
2025-12-11 01:04:54 -0400

    version final 2

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import pb from '../lib/pb'
 import Login from '../pages/Login.vue';
 import Register from '../pages/Register.vue';
 import Tasks from '../pages/Tasks.vue';
+import ResetPassword from '../pages/ResetPassword.vue';
 import Profile from '../pages/Profile.vue';
 
 // Se crean las rutas
 const routes = [
     { path: '/login', component: Login },
     { path: '/register', component: Register },
+    { path: '/reset', component: ResetPassword },
     { 
         path: '/tasks', 
         component: Tasks,
@@ -23,18 +25,22 @@ const routes = [
     { path: '/:pathMatch(.*)*', redirect: '/login' }
 ];
 
+// Crear la instancia del router de Vue
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+    history: createWebHistory(), // Historial basado en el API de historial del navegador
+    routes     // Definición de rutas de la aplicación
 });
 
 // Middleware de errores de autenticación
 router.beforeEach((to, from, next) => {
+    // Obtiene al usuario actual desde PocketBase authStore
     const user = pb.authStore.model;
-
+    // Si la ruta requiere autenticación y no hay usuario válido
     if (to.meta.requiresAuth && (!user || !pb.authStore.isValid)) {
+        // devuelve al login
         next('/login');
     } else {
+        // Si el usuario está autenticado o la ruta no requiere login, continuar normalmente
         next();
     }
 });
